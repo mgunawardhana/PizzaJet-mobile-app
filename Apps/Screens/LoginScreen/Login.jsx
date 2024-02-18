7a3730c8b74c03643c1da1fb9dbd53f18af1789a
mGunawardhana <mrgunawardhana27368@gmail.com>
2024-02-18 14:56:37 +0530

    content update

--- Apps/Screens/LoginScreen/Login.jsx
+++ Apps/Screens/LoginScreen/Login.jsx
@@ -1,10 +1,22 @@
-import { View, Text } from 'react-native'
+import { View, Image, StyleSheet } from 'react-native'
 import React from 'react'
 
 export default function Login() {
   return (
-    <View>
-      <Text>Login</Text>
+    <View style={{alignItems:'center'}}>
+      <Image source={require('./../../../assets/images/login.png')} 
+      style={styles.LoginImage} />
     </View>
   )
 }
+
+// rns
+const styles = StyleSheet.create({
+  LoginImage: {
+    width: 230,
+    height: 450,
+    marginTop: 70,
+    borderWidth: 4,
+    borderColor: '#000',
+  }
+})
