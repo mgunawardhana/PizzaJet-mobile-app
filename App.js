487da4265c44aed5c873d255fab045d59f10a306
mGunawardhana <mrgunawardhana27368@gmail.com>
2024-02-23 19:21:01 +0530

    auth login setup

--- App.js
+++ App.js
@@ -1,14 +1,26 @@
 import {StatusBar, StyleSheet, View} from 'react-native';
 import Colors from './Apps/Utils/Colors';
 import Login from "./Apps/Screens/LoginScreen/Login";
+import { ClerkProvider, SignedIn, SignedOut } from '@clerk/clerk-expo';
 
 export default function App() {
     return (
-
+        <ClerkProvider publishableKey='pk_test_ZnJlc2gtc3F1aXJyZWwtNTEuY2xlcmsuYWNjb3VudHMuZGV2JA'>
         <View style={styles.container}>
             <Login/>
+
+            {/* Signed in components */}
+            <SignedIn>
+          <Text>You are Signed in</Text>
+        </SignedIn>
+
+        {/* Signed out components */}
+        <SignedOut>
+        <Text>You are Signed out</Text>
+        </SignedOut>
             <StatusBar style="auto"/>
-        </View>);
+        </View>
+        </ClerkProvider>);
 }
 
 const styles = StyleSheet.create({
